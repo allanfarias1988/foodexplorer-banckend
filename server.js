45bd88345a3a0d5fc38c326542c72b12d8285a92
allanfarias1988 <allan.digitaltec@gmail.com>
2024-05-14 20:54:08 -0300

    Config server file and add AppError classe

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
-import AppError from "./src/utils/AppError";
-import routes from "./src/routes";
+import AppError from "./src/utils/AppError.js";
+import routes from "./src/routes/index.js";
 
 const app = express();
 
@@ -11,19 +11,21 @@ app.use(express.json());
 app.use("/files", express.static("files"));
 app.use(routes);
 
-app.use(error, (error, req, res, next) => {
+app.use((error, req, res, next) => {
     if(error instanceof AppError){
         return res.status(error.statusCode).json({
             status: "error",
             message: error.message
         })
-    } 
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
 })
 
-return res.status(500).json({
-    status: "error",
-    message: "Internal server error"
-})
+
 
 const PORT = process.env.PORT || 3333;
 
